5405ffc223f3e898ae3eba4db0ee74e73b6c3b22
AndyWams <andrew.wams@gmail.com>
2022-06-23 08:35:09 +0100

    added message module

---
+++ src/app/portal/services/messages.service.ts
@@ -0,0 +1,91 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { catchError, map } from 'rxjs/operators';
+import { handleError } from 'src/app/services/apiErrorHandler';
+import { AuthService } from 'src/app/services/auth.service';
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class MessagesService {
+  constructor(private http: HttpClient, private authService: AuthService) {
+    this.authService.getChurchSlug();
+  }
+
+  deleteFromTrash(model: any): Observable<any> {
+    return this.http
+      .post<any>(
+        ` ${
+          environment.managementbaseUrl
+        }/${this.authService.getChurchSlug()}/departments/trash/delete`,
+        model
+      )
+      .pipe(
+        map((status) => status),
+        catchError(handleError)
+      );
+  }
+  fetchAllMessages(type?: string): Observable<any> {
+    return this.http
+      .get<any>(
+        `${
+          environment.managementbaseUrl
+        }/${this.authService.getChurchSlug()}/messages/${type}`
+      )
+      .pipe(catchError(handleError));
+  }
+  fetchAllFromTrash(type?: string): Observable<any> {
+    return this.http
+      .get<any>(
+        `${
+          environment.managementbaseUrl
+        }/${this.authService.getChurchSlug()}/messages/trash/${type}`
+      )
+      .pipe(catchError(handleError));
+  }
+  fetchMesaageDetails(id: any): Observable<any> {
+    return this.http
+      .get<any>(
+        environment.managementbaseUrl +
+          `/${this.authService.getChurchSlug()}/messages/${id}`
+      )
+      .pipe(catchError(handleError));
+  }
+
+  moveToTrash(model: any): Observable<any> {
+    return this.http.post<any>(
+      `${
+        environment.managementbaseUrl
+      }/${this.authService.getChurchSlug()}/messages/delete`,
+      model
+    );
+  }
+  restore(model: any): Observable<any> {
+    return this.http
+      .post<any>(
+        `${
+          environment.managementbaseUrl
+        }/${this.authService.getChurchSlug()}/messages/trash/restore`,
+        model
+      )
+      .pipe(
+        map((status) => status),
+        catchError(handleError)
+      );
+  }
+  sendMessage(model: any): Observable<any> {
+    return this.http
+      .post<any>(
+        ` ${
+          environment.managementbaseUrl
+        }/${this.authService.getChurchSlug()}/messages`,
+        model
+      )
+      .pipe(
+        map((status) => status),
+        catchError(handleError)
+      );
+  }
+}
