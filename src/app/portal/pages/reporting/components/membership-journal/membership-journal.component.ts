038f54e04b1063248c7560a312ab4adf4e62367a
Emuohwo <warriisaac@outlook.com>
2022-07-07 12:04:06 +0200

    Add membership journal ui

--- src/app/portal/pages/reporting/components/membership-journal/membership-journal.component.ts
+++ src/app/portal/pages/reporting/components/membership-journal/membership-journal.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { bool } from 'aws-sdk/clients/signer';
+import { ReportingService } from 'src/app/portal/services/reporting.service';
 
 @Component({
   selector: 'app-membership-journal',
@@ -6,10 +8,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./membership-journal.component.scss']
 })
 export class MembershipJournalComponent implements OnInit {
+  firstTimersList: any[] = [];
+  regularMemberList: any[] = [];
+  newConvertList: any[] = [];
+  _loading_: boolean = false;
 
-  constructor() { }
+  constructor(private reportService: ReportingService) { }
 
   ngOnInit(): void {
+    this.getMemberShipJournal()
+  }
+
+  getMemberShipJournal() {
+    this._loading_ = true
+    this.reportService
+    .fetchMembershipJournal('membership_journal').subscribe(
+      (res) => {
+        this._loading_ = false;
+        const { first_timers, members, new_converts } = res;
+        console.log('res', res)
+        this.firstTimersList = first_timers;
+        this.newConvertList= new_converts;
+        this.regularMemberList = members
+      }
+    )
   }
 
 }
---
+++ src/app/portal/services/reporting.service.ts
@@ -0,0 +1,30 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+import { handleError } from 'src/app/services/apiErrorHandler';
+import { AuthService } from 'src/app/services/auth.service';
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ReportingService {
+
+  constructor(
+    private http: HttpClient,
+    private authService: AuthService,
+  ) {
+    this.authService.getChurchSlug();
+  }
+
+  fetchMembershipJournal(type: any, date?: any): Observable<any> {
+    return this.http
+      .get<any>(
+        `${
+          environment.managementbaseUrl
+        }/${this.authService.getChurchSlug()}/reporting/${type}/?date=${date}`
+      )
+      .pipe(catchError(handleError));
+  }
+}
