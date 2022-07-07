5a241e92b4b73e8dec0fa45a9a0cbf09c5f1d495
Emuohwo <warriisaac@outlook.com>
2022-07-07 13:37:33 +0200

    export members and first timers

--- src/app/portal/pages/reporting/components/membership-journal/membership-journal.component.ts
+++ src/app/portal/pages/reporting/components/membership-journal/membership-journal.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { bool } from 'aws-sdk/clients/signer';
+import { ExportServiceService } from 'src/app/portal/services/export-service.service';
 import { ReportingService } from 'src/app/portal/services/reporting.service';
+import { printElement } from 'src/app/shared';
 
 @Component({
   selector: 'app-membership-journal',
@@ -12,8 +13,12 @@ export class MembershipJournalComponent implements OnInit {
   regularMemberList: any[] = [];
   newConvertList: any[] = [];
   _loading_: boolean = false;
+  _printElement = printElement;
 
-  constructor(private reportService: ReportingService) { }
+  constructor(
+    private reportService: ReportingService,
+    private exportService: ExportServiceService,
+  ) { }
 
   ngOnInit(): void {
     this.getMemberShipJournal()
@@ -32,6 +37,63 @@ export class MembershipJournalComponent implements OnInit {
         this.regularMemberList = members
       }
     )
+  }
+  exportFirstTimersToExcel(): void {
+    const edata: Array<any> = [];
+    const udt: any = {
+      data: [
+        { A: 'FIRST TIMERS DATA' }, //title
+        {
+          A: '#',
+          B: 'First Name',
+          C: 'Last Name',
+          D: 'Phone Number',
+          E: 'Membership Information',
+          F: 'Convert Status',
+        }, // table header
+      ],
+      skipHeader: true,
+    };
+    this.firstTimersList.forEach((data) => {
+      udt.data.push({
+        A: data.id,
+        B: data.first_name,
+        C: data.last_name,
+        D: data.phone,
+        E: data.membership_info,
+        F: data.convert_status,
+      });
+    });
+    edata.push(udt);
+    this.exportService.exportTableElmToExcel(edata, 'First Timer data')
+  }
+
+  exportRegularMembersToExcel(): void {
+    const edata: Array<any> = [];
+    const udt: any = {
+      data: [
+        { A: 'REGULAR MEMBERS JOURNAL DATA' }, // title
+        {
+          A: '#',
+          B: 'First Name',
+          C: 'Last Name',
+          D: 'Phone',
+          E: 'Email',
+        }, // table header
+      ],
+      skipHeader: true,
+    };
+    this.regularMemberList.forEach((data) => {
+      udt.data.push({
+        A: data.id,
+        B: data.user.first_name,
+        C: data.user.last_name,
+        D: data.user.phone,
+        E: data.user.email,
+      });
+    });
+    edata.push(udt);
+    this.exportService.exportTableElmToExcel(edata, 'Regular members Journal Data')
   }
 
 }
