78a061a77d53412295da97cde21ce323009337a3
Emuohwo <warriisaac@outlook.com>
2022-07-17 18:02:58 +0200

    Adjust fonts

--- src/app/home/components/header/header.component.ts
+++ src/app/home/components/header/header.component.ts
@@ -6,10 +6,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
+  showMenu:boolean = false;
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  toggleMenu() {
+    this.showMenu = !this.showMenu
   }
 
 }
