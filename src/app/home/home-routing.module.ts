173095555ef7cca83b11ae82807ca5508325b99f
AndyWams <andrew.wams@gmail.com>
2022-07-19 11:49:46 +0100

    update

--- src/app/home/home-routing.module.ts
+++ src/app/home/home-routing.module.ts
@@ -1,27 +1,29 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoggedInAuthGuard } from '../services/login-auth-guard.guard';
 import { ContactPageComponent } from './components/contact-page/contact-page.component';
 import { HomePageComponent } from './components/home-page/home-page.component';
 
 const routes: Routes = [
   {
     path: '',
+    canActivate: [LoggedInAuthGuard],
     component: HomePageComponent,
     data: {
-      title: 'Churchvest - home'
+      title: 'Churchvest - home',
     },
   },
   {
     path: 'contact',
     component: ContactPageComponent,
     data: {
-      title: 'Contact - Churchvest'
+      title: 'Contact - Churchvest',
     },
   },
 ];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class HomeRoutingModule { }
+export class HomeRoutingModule {}
