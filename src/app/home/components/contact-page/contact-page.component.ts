922351e31ed0988b335b9278d91fd05b54a1e68f
AndyWams <andrew.wams@gmail.com>
2022-07-19 10:52:53 +0100

    merge landing page

--- src/app/home/components/contact-page/contact-page.component.ts
+++ src/app/home/components/contact-page/contact-page.component.ts
@@ -6,15 +6,15 @@ import { HomeService } from 'src/app/services/home.service';
 @Component({
   selector: 'app-contact-page',
   templateUrl: './contact-page.component.html',
-  styleUrls: ['./contact-page.component.scss']
+  styleUrls: ['./contact-page.component.scss'],
 })
 export class ContactPageComponent implements OnInit {
   contactForm: FormGroup = new FormGroup({});
-  
+
   isBusy: boolean = false;
-  reasonOfContact: string[] = ["Enquiries", "Complaint", "Book a demo"]
-  modeOfContact: string[] = ["email", "phone"];
-  meansOfContact = "";
+  reasonOfContact: string[] = ['Enquiries', 'Complaint', 'Book a demo'];
+  modeOfContact: string[] = ['email', 'phone'];
+  meansOfContact = '';
 
   constructor(
     private formBuilder: FormBuilder,
@@ -24,57 +24,60 @@ export class ContactPageComponent implements OnInit {
     this.contactForm = this.formBuilder.group({
       church_address: ['', Validators.required],
       church_name: ['', Validators.required],
+      meansOfContact: [''],
       message: ['', Validators.required],
       subject: ['', Validators.required],
       name: ['', Validators.required],
       position: ['', Validators.required],
-      email: ['',],
+      email: [''],
       phone: [''],
-    })
+    });
   }
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
   onChangeMode(evt) {
-      this.meansOfContact = evt.value
+    this.meansOfContact = evt.value;
     if (this.meansOfContact == 'email') {
       this.contactForm.get('phone').clearValidators();
-      this.contactForm.get('email').setValidators([Validators.required, Validators.email])
-    } 
+      this.contactForm
+        .get('email')
+        .setValidators([Validators.required, Validators.email]);
+    }
     if (this.meansOfContact == 'phone') {
       this.contactForm.get('email').clearValidators();
-      this.contactForm.get('phone').setValidators([Validators.required])
+      this.contactForm.get('phone').setValidators([Validators.required]);
     }
   }
 
   onSubmit() {
+    if (this.meansOfContact == '') {
+      return;
+    }
 
-    if (this.meansOfContact == '') return
-    
-    if (this.contactForm.invalid) return;
+    if (this.contactForm.invalid) {
+      return;
+    }
 
     const payload = this.contactForm.value;
     if (this.meansOfContact == 'email') {
-      delete payload.phone
-    } 
+      delete payload.phone;
+    }
     if (this.meansOfContact == 'phone') {
-      delete  payload.email
+      delete payload.email;
     }
     this.isBusy = true;
     this.contactService.postContact(this.contactForm.value).subscribe(
-      
-        (res) => {
-          // const { message } = res;
-          this.isBusy = false
-          this.toastr.success('Message sent successfully', 'Message');
-          this.contactForm.reset();
-        },
-        (error) => {
-          this.isBusy = false;
-          this.toastr.error(error, 'Message')
-        }
-    )
+      (res) => {
+        this.isBusy = false;
+        this.toastr.success('Message sent successfully', 'Message');
+        this.contactForm.reset();
+      },
+      (error) => {
+        this.isBusy = false;
+        this.toastr.error(error, 'Message');
+        this.contactForm.reset();
+      }
+    );
   }
-
 }
