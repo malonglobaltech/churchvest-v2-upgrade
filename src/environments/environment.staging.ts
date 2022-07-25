85a1437085c8b7467bb0852c7a4ae909a1fed546
AndyWams <andrew.wams@gmail.com>
2022-07-25 12:41:10 +0100

    deploying to staging

--- src/environments/environment.staging.ts
+++ src/environments/environment.staging.ts
@@ -1,9 +1,10 @@
 export const environment = {
+  name: 'churchvest-staging',
   production: false,
   staging: true,
   managementbaseUrl: 'https://back.churchvest.com/api/management',
   mainUrl: 'https://back.churchvest.com/api',
 
-  hostingUrl: 'https://staging.churchvest.com',
+  hostingUrl: 'https://staging.churchvest.com/',
   paystack: 'pk_test_e15cdc83c8a6ea440c22f8a3e7074137c9b01538',
 };
