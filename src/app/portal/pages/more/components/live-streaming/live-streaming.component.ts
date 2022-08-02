4f8944b820b6afae9f36d3faf04f21e6dfb8853a
Emuohwo <warriisaac@outlook.com>
2022-08-02 14:43:56 +0100

    Add live-streaming instruction

--- src/app/portal/pages/more/components/live-streaming/live-streaming.component.ts
+++ src/app/portal/pages/more/components/live-streaming/live-streaming.component.ts
@@ -6,10 +6,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./live-streaming.component.scss']
 })
 export class LiveStreamingComponent implements OnInit {
+  activeStreamKey
 
   constructor() { }
 
   ngOnInit(): void {
   }
+
+  copyToClipboard(evt) {}
+
+  updateStreamKey() {}
 
 }
